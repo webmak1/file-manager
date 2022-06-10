05ab6d309900869c091b04d7b34951d69c64b874
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2022-06-10 17:43:49 +0300

    feat: add messages constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,10 @@
-export const MESSAGES = {};
+export const MESSAGES = {
+  WELCOME: `Welcome to the File Manager, /usernameToken/!`,
+  CURRENT_DIR: `You are currently in /pathToken/`,
+  BYE: `Thank you for using File Manager, /usernameToken/!`,
+};
+
+export const ERROR_MESSAGES = {
+  INV_INPUT: 'Invalid input',
+  OPERATION_FAILED: 'Operation failed',
+};
